b76b2f4d7a6f00f5523dad278f6f6ff7193898bf
ThSFernandes <Thsalles99@gmail.com>
2025-01-05 17:26:55 -0300

    Projeto final testado

--- app/src/main/java/app/com/gamenhamnham/JogoActivity.kt
+++ app/src/main/java/app/com/gamenhamnham/JogoActivity.kt
@@ -1,5 +1,6 @@
 package app.com.gamenhamnham
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
@@ -14,7 +15,8 @@ class JogoActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityJogoBinding
     private var jogadorAtual = 1 // 1 para jogador 1, 2 para jogador 2
-    private val tabuleiro = Array(3) { Array(3) { mutableListOf<Pair<Int, Int>>() } } // Cada célula contém uma lista de pares (tamanho da peça, jogador)
+    private val tabuleiro =
+        Array(3) { Array(3) { mutableListOf<Pair<Int, Int>>() } } // Cada célula contém uma lista de pares (tamanho da peça, jogador)
     private var selecioanarPeca: ImageView? = null  // Variável para armazenar a peça selecionada
     private val pecasJogador1 = intArrayOf(3, 3, 3) // [pequena, média, grande]
     private val pecasJogador2 = intArrayOf(3, 3, 3) // [pequena, média, grande]
@@ -29,7 +31,7 @@ class JogoActivity : AppCompatActivity() {
         binding = ActivityJogoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setupClickListeners()  // Configura os listeners de clique
+        gerenciarToquesPecas()  // Configura os listeners de clique
         atualizarTextoDeTurno()  // Atualiza o indicador de turno
 
         binding.backButton.setOnClickListener {
@@ -40,28 +42,50 @@ class JogoActivity : AppCompatActivity() {
         binding.buttonReset.setOnClickListener {
             reiniciarJogo()  // Chama a função para reiniciar o jogo
         }
+
+
     }
 
-    private fun setupClickListeners() {
+    private fun gerenciarToquesPecas() {
         // Configurar os cliques nas peças do jogador 1
         binding.jog1PiecesLayout.forEach { piece ->
             piece.setOnClickListener {
-                if (jogadorAtual == 1 && verificarPecasDisponiveis(1, piece)) {
-                    selecionarPeca(it as ImageView)
-                } else {
-                    Toast.makeText(this, "Não é sua vez ou você não tem mais peças deste tamanho!", Toast.LENGTH_SHORT).show()
+                if (jogadorAtual != 1) {
+                    Toast.makeText(this, "Não é sua vez!", Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener
                 }
+
+                if (!verificarPecasDisponiveis(1, piece)) {
+                    Toast.makeText(
+                        this,
+                        "Você não tem mais peças deste tamanho!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+
+                selecionarPeca(it as ImageView)
             }
         }
 
         // Configurar os cliques nas peças do jogador 2
         binding.jog2PiecesLayout.forEach { piece ->
             piece.setOnClickListener {
-                if (jogadorAtual == 2 && verificarPecasDisponiveis(2, piece)) {
-                    selecionarPeca(it as ImageView)
-                } else {
-                    Toast.makeText(this, "Não é sua vez ou você não tem mais peças deste tamanho!", Toast.LENGTH_SHORT).show()
+                if (jogadorAtual != 2) {
+                    Toast.makeText(this, "Não é sua vez!", Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener
                 }
+
+                if (!verificarPecasDisponiveis(2, piece)) {
+                    Toast.makeText(
+                        this,
+                        "Você não tem mais peças deste tamanho!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+
+                selecionarPeca(it as ImageView)
             }
         }
 
@@ -69,17 +93,25 @@ class JogoActivity : AppCompatActivity() {
         for (row in 0..2) {
             for (col in 0..2) {
                 val cellId = "cell_${row}${col}"
-                val cell = binding.root.findViewById<ImageView>(resources.getIdentifier(cellId, "id", packageName))
+                val cell = binding.root.findViewById<ImageView>(
+                    resources.getIdentifier(
+                        cellId,
+                        "id",
+                        packageName
+                    )
+                )
                 cell.setOnClickListener {
                     if (selecioanarPeca != null) {
                         gerenciarPecas(cell, selecioanarPeca!!, row, col)
                     } else {
-                        Toast.makeText(this, "Selecione uma peça primeiro!", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "Selecione uma peça primeiro!", Toast.LENGTH_SHORT)
+                            .show()
                     }
                 }
             }
         }
     }
+
 
     private fun selecionarPeca(piece: ImageView) {
         // Remove o destaque da peça previamente selecionada
@@ -87,12 +119,21 @@ class JogoActivity : AppCompatActivity() {
 
         // Atualiza a peça selecionada
         selecioanarPeca = piece
-        piece.setBackgroundColor(ContextCompat.getColor(this, R.color.teal_200)) // Destaque da peça atual
+        piece.setBackgroundColor(
+            ContextCompat.getColor(
+                this,
+                R.color.teal_200
+            )
+        ) // Destaque da peça atual
     }
 
     private fun gerenciarPecas(cell: ImageView, piece: ImageView, row: Int, col: Int) {
         if (finalizarRodada) {
-            Toast.makeText(this, "O jogo já terminou! Reinicie para jogar novamente.", Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                this,
+                "O jogo já terminou! Para jogar novamente aperte o botão acima.",
+                Toast.LENGTH_SHORT
+            ).show()
             return
         }
 
@@ -111,8 +152,9 @@ class JogoActivity : AppCompatActivity() {
         // Verificar se há uma peça existente na célula
         if (tabuleiro[row][col].isNotEmpty()) {
             val existingPiece = tabuleiro[row][col].last()
-            // Se a peça existente for menor, ela é "comida"
             if (existingPiece.first < pieceSize && existingPiece.second != jogadorAtual) {
+
+                tocarSomSobreposicao()
                 // "Comer" a peça - devolver ao jogador
                 val removedPieceSize = existingPiece.first
                 if (existingPiece.second == 1) {
@@ -130,43 +172,49 @@ class JogoActivity : AppCompatActivity() {
                     binding.jog2PiecesLayout[removedPieceSize].visibility = View.VISIBLE
                 }
 
-                // Exibir mensagem de que a peça foi comida
-                Toast.makeText(this, "A peça foi comida e voltou para a mão do jogador ${existingPiece.second}!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "A peça foi comida e voltou para a mão do jogador ${existingPiece.second}!",
+                    Toast.LENGTH_SHORT
+                ).show()
             } else if (existingPiece.first >= pieceSize) {
                 // Se a peça não for comida, é uma jogada inválida
-                Toast.makeText(this, "Você não pode colocar uma peça menor aqui!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "Você precisa escolher uma peça maior para posicionar!",
+                    Toast.LENGTH_SHORT
+                ).show()
                 return
             }
         }
 
-        // Adiciona a peça à célula
-        tabuleiro[row][col].add(Pair(pieceSize, jogadorAtual)) // Adiciona a peça ao tabuleiro com seu jogador associado
-        cell.setImageDrawable(piece.drawable) // Coloca a peça na célula do tabuleiro
+        tabuleiro[row][col].add(Pair(pieceSize, jogadorAtual))
+        cell.setImageDrawable(piece.drawable)
 
-        // Atualiza a quantidade de peças restantes
         if (jogadorAtual == 1) {
             pecasJogador1[pieceSize]--
-            atualizarContadorPecas(pecasJogador1, 1) // Atualiza os contadores de peças do jogador 1
+            atualizarContadorPecas(pecasJogador1, 1)
         } else {
             pecasJogador2[pieceSize]--
-            atualizarContadorPecas(pecasJogador2, 2) // Atualiza os contadores de peças do jogador 2
+            atualizarContadorPecas(pecasJogador2, 2)
         }
 
-        // Verifica se o jogo terminou por vitória ou empate
+        // Se a peça foi colocada, desmarque a seleção
+        selecioanarPeca?.setBackgroundColor(ContextCompat.getColor(this, R.color.transparent))
+        selecioanarPeca = null
+
         if (verificarVitoria()) {
             verificaVencedor() // Atualiza o placar quando um jogador vence
             Toast.makeText(this, "Jogador $jogadorAtual venceu!", Toast.LENGTH_LONG).show()
             finalizarRodada = true // Marca o jogo como finalizado
             finalizarJogo() // Chama a função para desabilitar as interações e exibir a mensagem
-            piece.setBackgroundColor(ContextCompat.getColor(this, R.color.transparent))
             return
         }
-
-        piece.setBackgroundColor(ContextCompat.getColor(this, R.color.transparent))
 
         // Alterna o turno
         mudarTurno()
     }
+
     private fun atualizarContadorPecas(playerPieces: IntArray, player: Int) {
         val smallCount = playerPieces[0]
         val mediumCount = playerPieces[1]
@@ -188,7 +236,8 @@ class JogoActivity : AppCompatActivity() {
         for (row in 0..2) {
             if (tabuleiro[row][0].isNotEmpty() && tabuleiro[row][0].last().second == jogadorAtual &&
                 tabuleiro[row][1].isNotEmpty() && tabuleiro[row][1].last().second == jogadorAtual &&
-                tabuleiro[row][2].isNotEmpty() && tabuleiro[row][2].last().second == jogadorAtual) {
+                tabuleiro[row][2].isNotEmpty() && tabuleiro[row][2].last().second == jogadorAtual
+            ) {
                 return true // Vitória na linha
             }
         }
@@ -197,7 +246,8 @@ class JogoActivity : AppCompatActivity() {
         for (col in 0..2) {
             if (tabuleiro[0][col].isNotEmpty() && tabuleiro[0][col].last().second == jogadorAtual &&
                 tabuleiro[1][col].isNotEmpty() && tabuleiro[1][col].last().second == jogadorAtual &&
-                tabuleiro[2][col].isNotEmpty() && tabuleiro[2][col].last().second == jogadorAtual) {
+                tabuleiro[2][col].isNotEmpty() && tabuleiro[2][col].last().second == jogadorAtual
+            ) {
                 return true // Vitória na coluna
             }
         }
@@ -205,14 +255,16 @@ class JogoActivity : AppCompatActivity() {
         // Verificar diagonal principal
         if (tabuleiro[0][0].isNotEmpty() && tabuleiro[0][0].last().second == jogadorAtual &&
             tabuleiro[1][1].isNotEmpty() && tabuleiro[1][1].last().second == jogadorAtual &&
-            tabuleiro[2][2].isNotEmpty() && tabuleiro[2][2].last().second == jogadorAtual) {
+            tabuleiro[2][2].isNotEmpty() && tabuleiro[2][2].last().second == jogadorAtual
+        ) {
             return true // Vitória na diagonal principal
         }
 
         // Verificar diagonal secundária
         if (tabuleiro[0][2].isNotEmpty() && tabuleiro[0][2].last().second == jogadorAtual &&
             tabuleiro[1][1].isNotEmpty() && tabuleiro[1][1].last().second == jogadorAtual &&
-            tabuleiro[2][0].isNotEmpty() && tabuleiro[2][0].last().second == jogadorAtual) {
+            tabuleiro[2][0].isNotEmpty() && tabuleiro[2][0].last().second == jogadorAtual
+        ) {
             return true // Vitória na diagonal secundária
         }
 
@@ -236,7 +288,6 @@ class JogoActivity : AppCompatActivity() {
 
         return false // Nenhuma condição de vitória encontrada
     }
-
 
 
     private fun verificaVencedor() {
@@ -278,7 +329,13 @@ class JogoActivity : AppCompatActivity() {
         for (row in 0..2) {
             for (col in 0..2) {
                 val cellId = "cell_${row}${col}"
-                val cell = binding.root.findViewById<ImageView>(resources.getIdentifier(cellId, "id", packageName))
+                val cell = binding.root.findViewById<ImageView>(
+                    resources.getIdentifier(
+                        cellId,
+                        "id",
+                        packageName
+                    )
+                )
                 cell.isClickable = true // Habilita novamente as células
                 cell.setImageDrawable(null)  // Limpa as imagens das células
             }
@@ -294,15 +351,24 @@ class JogoActivity : AppCompatActivity() {
         // Reiniciar as peças dos jogadores: Habilitar as peças para seleção novamente
         binding.jog1PiecesLayout.forEach { piece ->
             piece.isClickable = true // Tornar as peças do jogador 1 clicáveis
-            piece.setBackgroundColor(ContextCompat.getColor(this, R.color.transparent)) // Remover qualquer destaque de seleção anterior
+            piece.setBackgroundColor(
+                ContextCompat.getColor(
+                    this,
+                    R.color.transparent
+                )
+            ) // Remover qualquer destaque de seleção anterior
         }
 
         binding.jog2PiecesLayout.forEach { piece ->
             piece.isClickable = true // Tornar as peças do jogador 2 clicáveis
-            piece.setBackgroundColor(ContextCompat.getColor(this, R.color.transparent)) // Remover qualquer destaque de seleção anterior
+            piece.setBackgroundColor(
+                ContextCompat.getColor(
+                    this,
+                    R.color.transparent
+                )
+            ) // Remover qualquer destaque de seleção anterior
         }
     }
-
 
 
     private fun finalizarJogo() {
@@ -314,7 +380,13 @@ class JogoActivity : AppCompatActivity() {
         for (row in 0..2) {
             for (col in 0..2) {
                 val cellId = "cell_${row}${col}"
-                val cell = binding.root.findViewById<ImageView>(resources.getIdentifier(cellId, "id", packageName))
+                val cell = binding.root.findViewById<ImageView>(
+                    resources.getIdentifier(
+                        cellId,
+                        "id",
+                        packageName
+                    )
+                )
                 cell.isClickable = false
             }
         }
@@ -346,6 +418,14 @@ class JogoActivity : AppCompatActivity() {
             pecasJogador1[pieceSize] > 0
         } else {
             pecasJogador2[pieceSize] > 0
+        }
+    }
+
+    private fun tocarSomSobreposicao() {
+        val mediaPlayer = MediaPlayer.create(this, R.raw.engolindo)
+        mediaPlayer.start()
+        mediaPlayer.setOnCompletionListener {
+            it.release() // Libera os recursos após tocar
         }
     }
 }
--- app/src/main/java/app/com/gamenhamnham/MainActivity.kt
+++ app/src/main/java/app/com/gamenhamnham/MainActivity.kt
@@ -1,6 +1,7 @@
 package app.com.gamenhamnham
 
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import app.com.gamenhamnham.databinding.ActivityMainBinding
@@ -8,11 +9,17 @@ import app.com.gamenhamnham.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var mediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Inicializar e começar a música
+        mediaPlayer = MediaPlayer.create(this, R.raw.background_music)
+        mediaPlayer?.isLooping = true // Configura para repetir a música
+        mediaPlayer?.start()
 
         // Configurar o clique do botão "Start Game"
         binding.startButton.setOnClickListener {
@@ -25,6 +32,24 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, RegrasActivity::class.java)
             startActivity(intent)
         }
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        // Parar e liberar os recursos do MediaPlayer
+        mediaPlayer?.release()
+        mediaPlayer = null
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // Pausar a música quando a Activity estiver em pausa
+        mediaPlayer?.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // Continuar a música quando a Activity for retomada
+        mediaPlayer?.start()
     }
 }
